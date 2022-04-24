18fd162f2255836297ae94a2da9ae347292cbadb
Mateus-msj3 <mateussouzaads@outlook.com>
2022-04-24 11:28:13 -0300

    feat: implementation user controller

---
+++ user-api/src/main/java/com/microservices/userapi/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.microservices.userapi.controller;
+
+import com.microservices.userapi.model.dto.UserDTO;
+import com.microservices.userapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/user/")
+    public List<UserDTO> getUsers() {
+        List<UserDTO> userDTOS = userService.getAll();
+        return userDTOS;
+    }
+
+    @GetMapping("/user/{id}")
+    UserDTO findById(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
+    @PostMapping("/user")
+    UserDTO newUser(UserDTO userDTO) {
+        return userService.save(userDTO);
+    }
+
+    @GetMapping("/user/cpf/{cpf}")
+    UserDTO findByCpf(@PathVariable String cpf) {
+        return userService.findByCpf(cpf);
+    }
+
+    @DeleteMapping("/user/{id}")
+    UserDTO delete(@PathVariable Long id) {
+        return userService.delete(id);
+    }
+
+    @GetMapping("/user/search")
+    public List<UserDTO> queryByName(@RequestParam(name="name", required = true) String name) {
+        return userService.queryByName(name);
+    }
+}
