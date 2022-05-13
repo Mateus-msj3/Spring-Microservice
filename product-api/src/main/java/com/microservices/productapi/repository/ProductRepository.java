493b66ce53130aa3ad6140d1452478087f32f5b3
Mateus-msj3 <mateussouzaads@outlook.com>
2022-05-12 21:54:09 -0300

    layer repository

---
+++ product-api/src/main/java/com/microservices/productapi/repository/ProductRepository.java
@@ -0,0 +1,19 @@
+package com.microservices.productapi.repository;
+
+import com.microservices.productapi.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query(value = "select p "
+            + "from product p"
+            + "join category c on p.category.id = c.id"
+            + "where c.id = :categoryId ")
+    public List<Product> getProductByCategory(@Param("categoryId") Long categoryId);
+
+    public Product findByProductIdentifier(String productIdentifier);
+}
